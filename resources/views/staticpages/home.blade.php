e28feee2b8e4ba9d8109daaa04a799be8108b7fc
zhanghuizheng <394401270@qq.com>
2019-08-25 19:25:11 +0800

    初始化样式

--- resources/views/staticpages/home.blade.php
+++ resources/views/staticpages/home.blade.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Weibo App</title>
-</head>
-<body>
-<h1>主⻚</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel ⼊⻔教程</a> 的⽰例项⽬主⻚。
+        </p>
+        <p>
+            ⼀切，将从这⾥开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
+@stop
