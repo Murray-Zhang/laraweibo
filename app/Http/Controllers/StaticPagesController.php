0ac453ae7a74d42006bcbdd3c0cd10a5b9c9eb99
zhanghuizheng <394401270@qq.com>
2019-08-25 19:16:42 +0800

    基础页面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home()
+    {
+        return view('staticpages/home');
+    }
+
+    public function help()
+    {
+        return view('staticpages/help');
+    }
+
+    public function about()
+    {
+        return view('staticpages/about');
+    }
+}
