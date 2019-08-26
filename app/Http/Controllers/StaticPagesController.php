7a90802d46baa42d77412c04f8de6252ab774dd1
zhanghuizheng <394401270@qq.com>
2019-08-27 03:09:57 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('staticpages/home');
+        $feed_items = [];
+        if(Auth::check()){
+            $feed_items = Auth::User()->statuses()->orderBy('created_at', 'desc')->paginate(10);
+        }
+        return view('staticpages/home', compact('feed_items'));
     }
 
     public function help()
